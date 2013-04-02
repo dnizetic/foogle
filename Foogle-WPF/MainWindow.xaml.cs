b3dbf96b563769f895cfc91c17602dded29346c5
Denis Nizetic <denis_nizetic@hotmail.com>
2013-04-02 16:44:25 +0200

    Added PGSQL support / DB funcs to WPF.

--- Foogle-WPF/MainWindow.xaml.cs
+++ Foogle-WPF/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+
+using Npgsql;
+
 
 namespace Foogle_WPF
 {
@@ -23,6 +27,8 @@ namespace Foogle_WPF
         public MainWindow()
         {
             InitializeComponent();
+
+            InitialiseDatabaseConnection();
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
@@ -47,9 +53,76 @@ namespace Foogle_WPF
 
         }
 
-        private void SearchCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
 
+
+        /*
+         * SuggestSkills()
+         *
+         * Based on current text in the search box, this
+         * function returns a set of suggestions for the
+         * user.
+         *
+         * @param (String current_text) current text typed in the search box
+         * //@return (String[]) array of suggestions (strings)
+         */
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+
+        private void SuggestSkills(String current_text)
+        {
+            try
+            {
+                
+                string sql = "select * from skill where skill_tag LIKE '%" + current_text + "%';";
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+
+                ds.Reset();
+
+                da.Fill(ds);
+                // since it C# DataSet can handle multiple tables, we will select first
+                dt = ds.Tables[0];
+                // connect grid to DataTable
+                //dataGridView1.DataSource = dt;
+
+                String[] suggs = new String[10];
+
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.ToString());
+                throw;
+            }
         }
+
+
+
+        private void SearchCombo_TextChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show("I'm changed");
+        }
+
+
+
+        public static NpgsqlConnection conn = null;
+        private void InitialiseDatabaseConnection()
+        {
+            try
+            {
+                // PostgeSQL-style connection string
+                string connstring = String.Format("Server={0};Port={1};" +
+                    "User Id={2};Password={3};Database={4};",
+                    "localhost", "5432", "postgres",
+                    "alphaomega", "foogle");
+                
+                conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                
+            }  catch (Exception msg) {
+               
+                MessageBox.Show(msg.ToString());
+                throw;
+            }
+        }
+
     }
 }
