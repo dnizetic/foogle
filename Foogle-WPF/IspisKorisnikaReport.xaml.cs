23b65a9b1f7cd68ab229fc0f12e26a7a4f4e1b93
Denis Nizetic <denis_nizetic@hotmail.com>
2013-06-23 18:24:59 +0200

    Reports updated

--- Foogle-WPF/IspisKorisnikaReport.xaml.cs
+++ Foogle-WPF/IspisKorisnikaReport.xaml.cs
@@ -5,13 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.IO;
+
 
 namespace Foogle_WPF
 {
@@ -26,11 +21,62 @@ namespace Foogle_WPF
             ispisKorisnika.Load += ispisKorisnika_Load;
         }
 
+
+        private bool _isReportViewerLoaded;
         void ispisKorisnika_Load(object sender, EventArgs e)
         {
+
+            //MessageBox.Show("HI");
             
             //ispisKorisnika.LocalReport.ReportEmbeddedResource = "TheApp.Reports.MyReport.rdlc";
-            ispisKorisnika.LocalReport.ReportEmbeddedResource = "foogle.Foogle-WPF.IspisKorisnika_report.rdlc";
+            //ispisKorisnika.LocalReport.ReportEmbeddedResource = "foogle.Foogle-WPF.IspisKorisnika_report.rdlc";
+
+            try
+            {
+
+
+                Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+                FoogleDS dataset = new FoogleDS();
+            
+                //AdventureWorks2008R2DataSet dataset = new AdventureWorks2008R2DataSet();
+
+                dataset.BeginInit();
+
+                reportDataSource1.Name = "ispis_korisnika_report"; //Name of the report dataset in our .RDLC file
+
+                reportDataSource1.Value = dataset.foogle_user;
+                //reportDataSource1.Value = dataset.SalesOrderDetail;
+                this.ispisKorisnika.LocalReport.DataSources.Add(reportDataSource1);
+
+                //this.ispisKorisnika.LocalReport.ReportEmbeddedResource = "Foogle-WPF.IspisKorisnika_report.rdlc";
+
+                //tvoja putanja (mozemo kasnije slozit ovo nije bitno)
+                this.ispisKorisnika.LocalReport.ReportPath = "C:/Users/denis/foogle/Foogle-WPF/IspisKorisnika_report.rdlc";
+                using (StreamReader rdlcSR = new StreamReader(@"C:/Users/denis/foogle/Foogle-WPF/IspisKorisnika_report.rdlc"))
+                {
+                    ispisKorisnika.LocalReport.LoadReportDefinition(rdlcSR);
+
+                    //ispisKorisnika.LocalReport.Refresh();
+                }
+            
+
+                //ispisKorisnika.LocalReport.ReportEmbeddedResource = "Foogle-WPF.IspisKorisnika_report.rdlc";
+            
+                dataset.EndInit();
+
+                //fill data into adventureWorksDataSet
+
+                FoogleDSTableAdapters.foogle_userTableAdapter adapter = new FoogleDSTableAdapters.foogle_userTableAdapter();
+   
+                adapter.ClearBeforeFill = true;
+                adapter.Fill(dataset.foogle_user);
+                ispisKorisnika.RefreshReport();
+
+                _isReportViewerLoaded = true;
+            }
+            catch (Exception ee)
+            {
+            }
         }
 
      
--- Foogle-WPF/MainWindow.xaml.cs
+++ Foogle-WPF/MainWindow.xaml.cs
@@ -444,6 +444,8 @@ namespace Foogle_WPF
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             IspisKorisnikaReport ik = new IspisKorisnikaReport();
+
+            
             ik.Show();
         }
 
